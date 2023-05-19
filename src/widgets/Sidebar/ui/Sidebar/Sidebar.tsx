93919422f1b4c8e779119c1b4484ff61b5ead91b
vladimirtsaran <vladimir.tsaran.x@gmail.com>
2023-05-19 17:35:04 +0200

    fixed linting Sidebar.tsx

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,34 +3,33 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from "react-i18next";
 import cls from './Sidebar.module.scss';
-import {useTranslation} from "react-i18next";
 
 interface SidebarProps {
     className?: string;
 }
-
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     return (
         <div
             data-testid="sidebar"
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            className={ classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className]) }
         >
             <Button
                 data-testid="sidebar-toggle"
-                onClick={onToggle}
+                onClick={ onToggle }
             >
-                {t('Переключить')}
+                { t('Переключить') }
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <LangSwitcher className={ cls.lang } />
             </div>
         </div>
     );
